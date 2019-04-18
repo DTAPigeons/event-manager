60b718d2941fc85a081d152f7561c1d3fe41caf5
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2019-04-18 18:43:26 +0300

    Added base event class.

---
+++ EventManger/EventManger/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManger {
+    public abstract class Event {
+
+    }
+}
