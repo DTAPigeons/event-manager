804aac72f4c08e1eb9ce84ee0e311ab02bc7f4e0
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2019-04-18 18:43:53 +0300

    Added some basic exeptions.

---
+++ EventManger/EventManger/Exceptions/InvalidEventTypeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManger.Exceptions {
+     public class InvalidEventTypeException: EventManagerExeption {
+        private Type invalidType;
+        public InvalidEventTypeException(Type invalidType) {
+            this.invalidType = invalidType;
+        }
+
+        public override string Message => $"The type {invalidType} is not a valid Event type!";
+    }
+}
