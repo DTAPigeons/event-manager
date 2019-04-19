a02da3f7dce572555b99e3b01cc283d984291a9a
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2019-04-19 17:28:04 +0300

    Finished functionality of Event Manager.

--- EventManger/EventManger/EventManager.cs
+++ EventManger/EventManger/EventManager.cs
@@ -45,18 +45,108 @@ namespace EventManger {
             abortedEvents.Clear();
         }
 
-        public static void AddHandler(Type eventType, EventDelegate handler, EventHandlerPriority priority = EventHandlerPriority.Default) {
+        public static void AddEventHandler(Type eventType, EventDelegate handler, EventHandlerPriority priority = EventHandlerPriority.Default) {
             if (!eventType.IsSubclassOf(typeof(Event))) {
                 throw new InvalidEventTypeException(eventType);
             }
             eventHandlersToAdd[(int)priority].Add(new KeyValuePair<Type, EventDelegate>(eventType,handler));
         }
 
-        public static void RemoveHandler(Type eventType, EventDelegate handler) {
+        public static void RemoveEventHandler(Type eventType, EventDelegate handler) {
             if (!eventType.IsSubclassOf(typeof(Event))) {
                 throw new InvalidEventTypeException(eventType);
             }
             eventHandlersToRemove.Add(new KeyValuePair<Type, EventDelegate>(eventType, handler));
+        }
+
+        public static void QueueEvent(Event newEvent)
+        {
+            newEvents.Enqueue(newEvent);
+        }
+
+        public static void AbortEvent(Event toAbort)
+        {
+            abortedEvents.Add(toAbort);
+        }
+
+        public static void Update()
+        {
+            RemoveHandlers();
+            AddHandlers();
+            TransferEvents();
+        }
+
+        private static void RemoveHandlers()
+        {
+            foreach(var pair in eventHandlersToRemove)
+            {
+                foreach(var priority in eventHandlers)
+                {
+                    if (priority.ContainsKey(pair.Key))
+                    {
+                        priority[pair.Key] -= pair.Value;
+                    }
+                }
+            }
+        }
+
+        private static void AddHandlers()
+        {
+            for (int i = 0; i < (int)EventHandlerPriority.Count; i++)
+            {
+                foreach (var pair in eventHandlersToAdd[i])
+                {
+                    if (eventHandlers[i].ContainsKey(pair.Key))
+                    {
+                        eventHandlers[i][pair.Key] -= pair.Value;        //Makes sure the same event handler cannot be added twice to the same event type
+                        eventHandlers[i][pair.Key] += pair.Value;
+                    }
+                    else
+                    {
+                        eventHandlers[i].Add(pair.Key, pair.Value);
+                    }
+                }
+                eventHandlersToAdd[i].Clear();
+            }            
+        }
+
+        private static void TransferEvents()
+        {
+            while (newEvents.Count > 0)
+            {
+                currentEvents.Enqueue(newEvents.Dequeue());
+            }
+        }
+
+        private static void ProcessEvents()
+        {
+            Event eventToProcess = null;
+            while (currentEvents.Count > 0)
+            {
+                eventToProcess = currentEvents.Dequeue();
+                if(abortedEvents.Any(e=> e.GetType() == eventToProcess.GetType()))
+                {
+                    continue;
+                }
+
+                DispatchEvent(eventToProcess);
+            }
+            abortedEvents.Clear();
+        }
+
+        private static void DispatchEvent(Event @event)
+        {
+            DispatchEventToHandlerByPriority(@event, EventHandlerPriority.High);
+            DispatchEventToHandlerByPriority(@event, EventHandlerPriority.Default);
+            DispatchEventToHandlerByPriority(@event, EventHandlerPriority.Low);
+        }
+
+        private static void DispatchEventToHandlerByPriority(Event @event, EventHandlerPriority priority)
+        {
+            if (eventHandlers[(int)priority].ContainsKey(@event.GetType()))
+            {
+                eventHandlers[(int)priority][@event.GetType()]?.Invoke(@event);
+            }
         }
     }
 }
